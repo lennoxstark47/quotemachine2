ee6432316a3e2fff035a0dcbab626c6b30ebcc13
Stark 47 <twisamhaldar@gmail.com>
2021-04-17 23:21:10 +0530

    phase 3

--- src/quotemachine.jsx
+++ src/quotemachine.jsx
@@ -3,7 +3,9 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-// import Button from '@material-ui/core/Button';
+import { faQuoteLeft } from '@fortawesome/free-solid-svg-icons/';
+// import { faQuoteLeft } from '@fortawesome/fontawesome-free/svgs/solid';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 const QuoteMachine = ({
 	selectedQuote,
@@ -19,16 +21,24 @@ const QuoteMachine = ({
 							color: selectedColor
 								? `${selectedColor.hex}`
 								: 'blue',
+							transition: 'color 1s ease',
 						}}>
 						<div style={{ padding: '30px' }}>
 							<Typography variant='h4'>
+								<FontAwesomeIcon
+									icon={faQuoteLeft}
+									size='md'></FontAwesomeIcon>
 								{selectedQuote.quote}
 								<div
 									style={{
 										textAlign: 'right',
-										fontSize: '15px',
+										fontSize: '20px',
 									}}>
-									<span>
+									<span
+										style={{
+											fontFamily: 'Raleway',
+											fontWeight: '500',
+										}}>
 										-{selectedQuote.author}
 									</span>
 								</div>
@@ -37,30 +47,33 @@ const QuoteMachine = ({
 					</div>
 				) : null}
 			</CardContent>
-
-			<CardActions>
-				<button
-					className='btn'
-					onClick={handleClick}
-					style={{
-						height: '38px',
-						border: 'none',
-						borderRadius: '3px',
-						backgroundColor: selectedColor
-							? `${selectedColor.hex}`
-							: null,
-						color: 'white',
-						outline: 'none',
-						fontSize: '0.85em',
-						padding: '8px 18px 6px 18px',
-						marginTop: '30px',
-						cursor: 'pointer',
-						fontFamily: 'Raleway',
-						fontWeight: '500',
-					}}>
-					New Quote
-				</button>
-			</CardActions>
+			<div style={{ float: 'right' }}>
+				<CardActions>
+					<button
+						className='btn'
+						onClick={handleClick}
+						style={{
+							height: '38px',
+							border: 'none',
+							borderRadius: '3px',
+							backgroundColor: selectedColor
+								? `${selectedColor.hex}`
+								: null,
+							color: 'white',
+							outline: 'none',
+							fontSize: '0.85em',
+							padding: '8px 18px 6px 18px',
+							marginTop: '30px',
+							cursor: 'pointer',
+							fontFamily: 'Raleway',
+							fontWeight: '500',
+							transition:
+								'background-color 1s ease',
+						}}>
+						New Quote
+					</button>
+				</CardActions>
+			</div>
 		</Card>
 	);
 };
